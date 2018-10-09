5f9e65742aa89c1d8f8905c6b66ab6cabf63486a
lagicrus <Lagicrus@users.noreply.github.com>
2018-10-09 12:00:20 +0100

    Revert "main"
    This reverts commit ecc5ead5b8f43d4119377a22d20f7ce3b8b7c5e6.

--- index.js
+++ index.js
@@ -12,109 +12,16 @@
 'use strict';
 
 function id() {
-    return "UP857256";
+    return "Replace This With Your Student ID";
     // e.g. return "UP654321";
 }
 
 function fn() {
-    return "Sam";
+    return "Replace This With Your First Name";
 }
 
 function sn() {
-    return "Riches";
+    return "Replace This With Your Surname";
 }
 
-function replaceText(element, string) {
-    element.textContent = string;
-}
-
-function addTextTo(element, string) {
-    element.textContent = element.textContent.concat(string)
-}
-
-function moreBears() {
-    const element = document.getElementById("animals");
-    element.src = "http://placebear.com/400/200";
-    element.alt = "A bear.";
-    element.title = "A BEAR!"
-}
-
-function setId(element, string){
-    element.id = string;
-    return element
-}
-
-function setClass(element, string){
-    element.className = string;
-    return element
-}
-
-function addAClass(element, string){
-    element.classList.add(string);
-    return element
-}
-
-function removeAClass(element, string){
-    element.classList.remove(string);
-    return element
-}
-
-function newElement(name){
-    return document.createElement(name)
-}
-
-function findElementById(id){
-    return document.getElementById(id);
-}
-
-function findElementsByQuery(query){
-    return document.querySelectorAll(query)
-}
-
-function reverseList(selector){
-    const element = document.querySelector(selector);
-    let length = element.childNodes.length;
-    while (length--){
-        element.appendChild(element.childNodes[length]);
-    }
-    return element
-}
-
-function mover(selector, move_to_selector){
-    const element = document.querySelector(selector);
-    const move_to = document.querySelector(move_to_selector);
-    move_to.appendChild(element)
-}
-
-function filler(list_element, array){
-    for (const property of array){
-        const li = document.createElement("li");
-        li.textContent = property;
-        list_element.appendChild(li);
-    }
-    return list_element
-}
-
-function dupe(selector){
-    const element = document.querySelector(selector);
-    const new_element = document.createElement("p");
-    new_element.textContent = element.textContent;
-    element.parentNode.appendChild(new_element);
-}
-
-function removeAll(selector){
-    console.log(selector);
-    const elements = document.querySelectorAll(selector);
-    for (const element of elements){
-        element.parentElement.removeChild(element)
-    }
-}
-
-function getUserData(){
-    const username = document.querySelector("#username");
-    const speed = document.querySelector("#speed");
-    const student = document.querySelector("#student");
-    return {name: username.value,
-            speed: parseInt(speed.value),
-            student: student.checked}
-}
+// add your functions here
